e3ba9203e0b91e5848388821bf54f5cb52108b6e
wenmz6 <113717831+wenmz6@users.noreply.github.com>
2025-02-26 08:41:05 +0100

    Add files via upload

---
+++ Boucles.java
@@ -0,0 +1,56 @@
+package découverte;
+
+import java.util.Scanner;
+
+public class Boucles {
+
+    public static void main(String[] args) {
+        int cpt=0;
+        int nb;
+        while(cpt<=10) {
+            System.out.println(cpt);
+            //cpt=cpt+1;
+            cpt++;
+        }
+        System.out.println("après ma boucle cpt="+cpt);
+        do {
+            System.out.println(cpt);
+            //cpt=cpt-1;
+            //cpt--;
+            //cpt=cpt-2;
+            cpt-=2;
+        }while(cpt>=0);
+        System.out.println("après ma boucle cpt="+cpt);
+        for(int i=0; i<10;i++) {
+            System.out.println("i="+i);
+        }
+        // System.out.println("i="+i); 
+        // ko: i est déclaré dans la boucle donc utilisable dans la boucle uniquement
+    
+        // pour saisir un nombre pair
+        do {
+            System.out.println("entrez un nombre pair");
+            nb= new Scanner(System.in).nextInt();
+        }while(nb%2!=0);
+        
+        
+        System.out.println("entrez un nombre pair");
+        nb= new Scanner(System.in).nextInt();
+        while(nb%2!=0) {
+            System.out.println("entrez un nombre pair");
+            nb= new Scanner(System.in).nextInt();
+        }
+        
+        nb= 1;
+        while(nb%2!=0) {
+            System.out.println("entrez un nombre pair");
+            nb= new Scanner(System.in).nextInt();
+        }
+        
+        
+        
+        
+    
+    }
+
+}
